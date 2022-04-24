9aa015cb687a3e9016ced10aa498d9ec588d6764
Harm Smits <harmsmitsdev@gmail.com>
2022-04-24 10:32:32 +0200

    Overall fixes, p2 now works again after CentOS deprecation

--- part2/scripts/master_setup.sh
+++ part2/scripts/master_setup.sh
@@ -7,13 +7,7 @@ echo "alias k='k3s kubectl'" >> /etc/profile.d/00-aliases.sh
 
 sudo yum install net-tools -y
 
-/usr/local/bin/kubectl create configmap app1-html --from-file /vagrant/kubernetes/app1/index.html
-/usr/local/bin/kubectl apply -f /vagrant/kubernetes/app1/deployment-svc.yaml
-
-/usr/local/bin/kubectl create configmap app2-html --from-file /vagrant/confs/k3s/app2/index.html
-/usr/local/bin/kubectl apply -f /vagrant/kubernetes/app2/deployment-svc.yaml
-
-/usr/local/bin/kubectl create configmap app3-html --from-file /vagrant/confs/k3s/app3/index.html
-/usr/local/bin/kubectl apply -f /vagrant/kubernetes/app3/deployment-svc.yaml
-
+/usr/local/bin/kubectl apply -f /vagrant/kubernetes/app1.yaml
+/usr/local/bin/kubectl apply -f /vagrant/kubernetes/app2.yaml
+/usr/local/bin/kubectl apply -f /vagrant/kubernetes/app3.yaml
 /usr/local/bin/kubectl apply -f /vagrant/kubernetes/ingress.yaml
